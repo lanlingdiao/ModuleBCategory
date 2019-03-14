6f2be0511ddbcb44a61ac08ec687fb736afba5b2
lanlingdiao <lansebidiao@163.com>
2019-03-14 21:45:08 +0800

    Add files via upload

---
+++ ModuleBCategory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ModuleBCategory
+//
+//  Created by Azzan on 2018/12/2.
+//  Copyright © 2018年 Azzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ModuleBCategory/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModuleBCategory
+//
+//  Created by Azzan on 2018/12/2.
+//  Copyright © 2018年 Azzan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
